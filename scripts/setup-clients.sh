370b67000a0e8162d21bfdce3c1ed25757c7c870
Garðar Þorsteinsson <gardart@gmail.com>
2023-09-02 14:37:18 +0000

    Update setup-clients.sh

--- scripts/setup-clients.sh
+++ scripts/setup-clients.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Go to https://github.com/openshift/okd/releases/, replace version
 
-VERSION=4.5.0-0.okd-2020-10-15-235428
+#VERSION=4.5.0-0.okd-2020-10-15-235428
+VERSION=4.13.0-0.okd-2023-08-18-135805
 
 wget https://github.com/openshift/okd/releases/download/$VERSION/openshift-client-linux-$VERSION.tar.gz
 wget https://github.com/openshift/okd/releases/download/$VERSION/openshift-install-linux-$VERSION.tar.gz
